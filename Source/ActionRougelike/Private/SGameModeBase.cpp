6e4e48318ccc159379d9757c76228b28e3f5f66a
jemondey <105375003+jemondey@users.noreply.github.com>
2024-02-07 14:23:44 +0600

    Lecture 12 - More AI, Environment Query Spawn Logic
    - Added GameMode with custom AI spawn behavior
    - Added EQS to find bot spawnpoints

---
+++ Source/ActionRougelike/Private/SGameModeBase.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SGameModeBase.h"
+#include "EnvironmentQuery/EnvQueryManager.h"
+#include "EnvironmentQuery/EnvQueryInstanceBlueprintWrapper.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "AI/SAICharacter.h"
+#include "SAttributeComponent.h"
+#include "EngineUtils.h"
+
+
+ASGameModeBase::ASGameModeBase()
+{
+	SpawnTimerInterval = 2.0f;
+}
+
+void ASGameModeBase::StartPlay()
+{
+	Super::StartPlay();
+
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle_SpawnBots, this, &ASGameModeBase::SpawnBotTimerElapsed, SpawnTimerInterval, true);
+}
+
+void ASGameModeBase::SpawnBotTimerElapsed()
+{
+	UEnvQueryInstanceBlueprintWrapper* QueryInstance = UEnvQueryManager::RunEQSQuery(this, SpawnBotQuery, this, EEnvQueryRunMode::RandomBest5Pct, nullptr);
+	if (QueryInstance)
+	{
+		QueryInstance->GetOnQueryFinishedEvent().AddDynamic(this, &ASGameModeBase::OnQueryFinished);
+	}
+}
+
+void ASGameModeBase::OnQueryFinished(UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus)
+{
+	if (QueryStatus != EEnvQueryStatus::Success)
+	{
+		return;
+	}
+
+	int32 NrOfAliveBots = 0;
+	for (ASAICharacter* Bot : TActorRange<ASAICharacter>(GetWorld()))
+	{
+		USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(Bot->GetComponentByClass(USAttributeComponent::StaticClass()));
+		if (AttributeComp && AttributeComp->IsAlive())
+		{
+			NrOfAliveBots++;
+		}
+	}
+
+	float MaxBotAllowed = 10.f;
+	if (ensure(DifficultyCurve))
+	{
+		MaxBotAllowed = DifficultyCurve->GetFloatValue(GetWorld()->TimeSeconds);
+	}
+
+	if (NrOfAliveBots >= MaxBotAllowed)
+	{
+		return;
+	}
+
+	
+
+	TArray<FVector> Locations = QueryInstance->GetResultsAsLocations();
+	if (Locations.Num() > 0)
+	{
+		GetWorld()->SpawnActor<AActor>(MinionClass, Locations[0], FRotator::ZeroRotator);
+	}
+}
---
+++ Source/ActionRougelike/Public/SGameModeBase.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+#include "SGameModeBase.generated.h"
+
+UCLASS()
+class ACTIONROUGELIKE_API ASGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+
+	ASGameModeBase();
+
+	virtual void StartPlay() override;
+
+protected:
+
+	FTimerHandle TimerHandle_SpawnBots;
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	float SpawnTimerInterval;
+
+	void SpawnBotTimerElapsed();
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	class UEnvQuery* SpawnBotQuery;
+
+	UFUNCTION()
+	void OnQueryFinished(class UEnvQueryInstanceBlueprintWrapper* QueryInstance, EEnvQueryStatus::Type QueryStatus);
+
+	UPROPERTY(EditDefaultsOnly, Category = "AI")
+	TSubclassOf<AActor> MinionClass;
+
+	UPROPERTY(EditAnywhere, Category = "AI")
+	UCurveFloat* DifficultyCurve;
+
+};
